835ea7eda265f39fe717d7b22f3060f38846105b
Vitaly K <vitaly.kizner@ya.ru>
2022-07-01 01:16:33 +0700

    redact

--- src/test/java/goodline/FirstTest.java
+++ src/test/java/goodline/FirstTest.java
@@ -10,13 +10,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
-import static jdk.internal.agent.Agent.getText;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 
 public class FirstTest {
 
@@ -24,7 +17,6 @@ public class FirstTest {
     public void firstTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
-
         driver.get("https://testsheepnz.github.io/BasicCalculator.html");
         //driver.navigate().to("https://testsheepnz.github.io/BasicCalculator.html");
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
@@ -34,18 +26,9 @@ public class FirstTest {
 
         new Select(driver.findElement(By.id("selectOperationDropdown"))).selectByVisibleText("Subtract");
         driver.findElement(By.id("calculateButton")).click();
-
-
         Thread.sleep(10000);
-        String value = String.valueOf(driver.findElement(By.xpath(/[; @id="numberAnswerField"])).getText());
-        System.out.println(value);
-
-        //assertTrue(value.equals("-1"));
-
-        //Objects.equals(value, new String("-1"));
-
-
-        //driver.quit();
+        String value = driver.findElement(By.id("numberAnswerField")).getAttribute("value");
+        driver.quit();
     }
 
 }

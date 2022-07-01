7d5381d4c360a7c158955905e77ec3100aca8208
Vitaly K <vitaly.kizner@ya.ru>
2022-07-01 10:48:55 +0700

    new tests

--- src/test/java/goodline/SecondTest.java
+++ src/test/java/goodline/SecondTest.java
@@ -1,4 +1,26 @@
 package goodline;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class SecondTest {
+
+    @Test
+    public void secondTest() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:/driver/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://testsheepnz.github.io/BasicCalculator.html");
+        Thread.sleep(5000);
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        new Select(driver.findElement(By.id("selectBuild"))).selectByVisibleText("Prototype");
+        driver.findElement(By.id("number1Field")).sendKeys("gs");
+        driver.findElement(By.id("number2Field")).sendKeys("bu");
+        new Select(driver.findElement(By.id("selectOperationDropdown"))).selectByVisibleText("Concatenate");
+        driver.findElement(By.id("calculateButton")).click();
+        String value = driver.findElement(By.id("numberAnswerField")).getAttribute("value");
+        driver.quit();
+    }
 }
